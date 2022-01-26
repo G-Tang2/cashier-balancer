9d9c32b0188b01e4957f0c539504b90e0408ae66
Garvin <gtan36@student.monash.edu>
2022-01-26 15:54:57 +1100

    Changed till to use cash spinbox

--- lib/page/till.dart
+++ lib/page/till.dart
@@ -1,6 +1,7 @@
 import 'package:app/main.dart';
 import 'package:app/provider/till_provider.dart';
 import 'package:app/widget/bottom_navigation.dart';
+import 'package:app/widget/cash_spinbox.dart';
 import 'package:app/widget/message_box.dart';
 import 'package:app/widget/next_button.dart';
 import 'package:app/widget/till_horizontal_spinbox.dart';
@@ -26,21 +27,76 @@ class _TillPageState extends State<TillPage> {
       body: Column(children: [
         Expanded(
             child: Scrollbar(
-                child: ListView(children: const [
-          MessageBox(Text(
+                child: ListView(children: [
+          const MessageBox(Text(
               'Enter the number coins and notes for each denomination.',
               style: TextStyle(color: Colors.black))),
-          TillHorizontalSpinBox(0, '\$0.05', CashType.coin),
-          TillHorizontalSpinBox(1, '\$0.10', CashType.coin),
-          TillHorizontalSpinBox(2, '\$0.20', CashType.coin),
-          TillHorizontalSpinBox(3, '\$0.50', CashType.coin),
-          TillHorizontalSpinBox(4, '\$1.00', CashType.coin),
-          TillHorizontalSpinBox(5, '\$2.00', CashType.coin),
-          TillHorizontalSpinBox(0, '\$5.00', CashType.note),
-          TillHorizontalSpinBox(1, '\$10.00', CashType.note),
-          TillHorizontalSpinBox(2, '\$20.00', CashType.note),
-          TillHorizontalSpinBox(3, '\$50.00', CashType.note),
-          TillHorizontalSpinBox(4, '\$100.00', CashType.note)
+          CashSpinBox(
+            0,
+            '\$0.05',
+            context.watch<TillModel>().getCoinCount,
+            context.read<TillModel>().setCoinCount,
+          ),
+          CashSpinBox(
+            1,
+            '\$0.10',
+            context.watch<TillModel>().getCoinCount,
+            context.read<TillModel>().setCoinCount,
+          ),
+          CashSpinBox(
+            2,
+            '\$0.20',
+            context.watch<TillModel>().getCoinCount,
+            context.read<TillModel>().setCoinCount,
+          ),
+          CashSpinBox(
+            3,
+            '\$0.50',
+            context.watch<TillModel>().getCoinCount,
+            context.read<TillModel>().setCoinCount,
+          ),
+          CashSpinBox(
+            4,
+            '\$1.00',
+            context.watch<TillModel>().getCoinCount,
+            context.read<TillModel>().setCoinCount,
+          ),
+          CashSpinBox(
+            5,
+            '\$2.00',
+            context.watch<TillModel>().getCoinCount,
+            context.read<TillModel>().setCoinCount,
+          ),
+          CashSpinBox(
+            0,
+            '\$5.00',
+            context.watch<TillModel>().getNoteCount,
+            context.read<TillModel>().setNoteCount,
+          ),
+          CashSpinBox(
+            1,
+            '\$10.00',
+            context.watch<TillModel>().getNoteCount,
+            context.read<TillModel>().setNoteCount,
+          ),
+          CashSpinBox(
+            2,
+            '\$20.00',
+            context.watch<TillModel>().getNoteCount,
+            context.read<TillModel>().setNoteCount,
+          ),
+          CashSpinBox(
+            3,
+            '\$50.00',
+            context.watch<TillModel>().getNoteCount,
+            context.read<TillModel>().setNoteCount,
+          ),
+          CashSpinBox(
+            4,
+            '\$100.00',
+            context.watch<TillModel>().getNoteCount,
+            context.read<TillModel>().setNoteCount,
+          )
         ]))),
         BottomNavigation(
             text: '\$${context.watch<TillModel>().getTotal.toStringAsFixed(2)}',
